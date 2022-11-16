ebad1d1e9c0bda27ff5d19f522110ac50d33847f
victortupinamba <101647391+victortupinamba@users.noreply.github.com>
2022-11-16 12:06:45 -0300

    1.2
    - Criamos as primeiras linhas do labirinto

---
+++ js/main.js
@@ -0,0 +1,65 @@
+var ex=document.getElementById("canvas");
+			var ctx=ex.getContext("2d");            
+			const PI=3.14;		
+        
+			ctx.moveTo(550,500);                 
+			ctx.lineTo(850,500);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();        
+			     
+			ctx.moveTo(500,500);                 
+			ctx.lineTo(500,100);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();   
+
+			ctx.moveTo(900,100);                 
+			ctx.lineTo(500,100);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();   
+
+			ctx.moveTo(900,500);                 
+			ctx.lineTo(900,100);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();   
+
+			ctx.moveTo(550,250);                 
+			ctx.lineTo(550,100);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();  
+
+			ctx.moveTo(550,500);                 
+			ctx.lineTo(550,400);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();  
+
+			ctx.moveTo(550,400);                 
+			ctx.lineTo(650,400);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();   
+
+			ctx.moveTo(600,400);                 
+			ctx.lineTo(600,470);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();   
+
+			
+			ctx.moveTo(590,300);                 
+			ctx.lineTo(590,400);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke(); 
+
+			ctx.moveTo(600,470);                 
+			ctx.lineTo(650,470);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke(); 
+
+			ctx.moveTo(590,300);                 
+			ctx.lineTo(700,300);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke(); 
+
+			
+			ctx.moveTo(550,250);                 
+			ctx.lineTo(600,250);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke(); 
