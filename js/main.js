7372f3aa63881d0c0a48f5790288340469b59d93
victortupinamba <101647391+victortupinamba@users.noreply.github.com>
2022-11-21 22:04:38 -0300

    1.4
    Desenho do labirinto completo

--- js/main.js
+++ js/main.js
@@ -22,8 +22,8 @@ var ex=document.getElementById("canvas");
 			ctx.strokeStyle= "black";               
 			ctx.stroke();   
 
-			ctx.moveTo(550,250);                 
-			ctx.lineTo(550,140);                 
+			ctx.moveTo(530,250);                 
+			ctx.lineTo(530,140);                 
 			ctx.strokeStyle= "black";               
 			ctx.stroke();  
 
@@ -70,12 +70,12 @@ var ex=document.getElementById("canvas");
 			ctx.stroke(); 
 
 			ctx.moveTo(800, 100);                 
-			ctx.lineTo(800,250);                 
+			ctx.lineTo(800,220);                 
 			ctx.strokeStyle= "black";               
 			ctx.stroke();
 
-			ctx.moveTo(700, 470);                 
-			ctx.lineTo(700,380);                 
+			ctx.moveTo(680, 470);                 
+			ctx.lineTo(680,380);                 
 			ctx.strokeStyle= "black";               
 			ctx.stroke();
 
@@ -89,23 +89,23 @@ var ex=document.getElementById("canvas");
 			ctx.strokeStyle= "black";               
 			ctx.stroke();
 
-			ctx.moveTo(670, 130);                 
+			ctx.moveTo(650, 130);                 
+			ctx.lineTo(650,250);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();
+
+			ctx.moveTo(670, 170);                 
 			ctx.lineTo(670,250);                 
 			ctx.strokeStyle= "black";               
 			ctx.stroke();
 
-			ctx.moveTo(720, 170);                 
-			ctx.lineTo(720,250);                 
+			ctx.moveTo(650, 250);                 
+			ctx.lineTo(670,250);                 
 			ctx.strokeStyle= "black";               
 			ctx.stroke();
 
-			ctx.moveTo(670, 250);                 
-			ctx.lineTo(720,250);                 
-			ctx.strokeStyle= "black";               
-			ctx.stroke();
-
-			ctx.moveTo(860, 250);                 
-			ctx.lineTo(800,250);                 
+			ctx.moveTo(820, 220);                 
+			ctx.lineTo(800,220);                 
 			ctx.strokeStyle= "black";               
 			ctx.stroke();
 
@@ -114,8 +114,8 @@ var ex=document.getElementById("canvas");
 			ctx.strokeStyle= "black";               
 			ctx.stroke();
 
-			ctx.moveTo(630, 350);                 
-			ctx.lineTo(720,350);                 
+			ctx.moveTo(610, 380);                 
+			ctx.lineTo(680,380);                 
 			ctx.strokeStyle= "black";               
 			ctx.stroke();
 
@@ -124,27 +124,206 @@ var ex=document.getElementById("canvas");
 			ctx.strokeStyle= "black";               
 			ctx.stroke();
 
-			ctx.moveTo(630, 140);                 
-			ctx.lineTo(630,300);                 
+			ctx.moveTo(600, 140);                 
+			ctx.lineTo(600,300);                 
 			ctx.strokeStyle= "black";               
 			ctx.stroke();
 
-			ctx.moveTo(720, 140);                 
-			ctx.lineTo(750,140);                 
+			ctx.moveTo(700, 140);                 
+			ctx.lineTo(730,140);                 
 			ctx.strokeStyle= "black";               
 			ctx.stroke();
 
-			ctx.moveTo(750, 140);                 
+			ctx.moveTo(700, 140);                 
+			ctx.lineTo(700,190);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();
+
+			ctx.moveTo(700, 190);                 
 			ctx.lineTo(750,190);                 
 			ctx.strokeStyle= "black";               
 			ctx.stroke();
 
 			ctx.moveTo(750, 190);                 
-			ctx.lineTo(770,190);                 
+			ctx.lineTo(750,250);                 
 			ctx.strokeStyle= "black";               
 			ctx.stroke();
 
-			ctx.moveTo(770, 190);                 
-			ctx.lineTo(770,250);                 
+			
+			ctx.moveTo(550, 300);                 
+			ctx.lineTo(590,300);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();
+
+			ctx.moveTo(550, 320);                 
+			ctx.lineTo(500,320);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();
+
+			ctx.moveTo(550, 340);                 
+			ctx.lineTo(590,340);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();
+
+			ctx.moveTo(550, 360);                 
+			ctx.lineTo(500,360);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();
+
+			ctx.moveTo(780, 300);                 
+			ctx.lineTo(780,480);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();
+
+			ctx.moveTo(830, 480);                 
+			ctx.lineTo(780,480);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();
+
+			ctx.moveTo(690, 300);                 
+			ctx.lineTo(690,360);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();
+
+			ctx.moveTo(610, 360);                 
+			ctx.lineTo(690,360);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();
+
+			ctx.moveTo(710, 400);                 
+			ctx.lineTo(710,500);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();
+
+			ctx.moveTo(680, 430);                 
+			ctx.lineTo(620,430);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();
+
+			ctx.moveTo(745,460);                 
+			ctx.lineTo(745,350);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();
+
+			ctx.moveTo(745, 370);                 
+			ctx.lineTo(720,370);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();
+
+			ctx.moveTo(880, 370);                 
+			ctx.lineTo(850,370);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();
+			
+			ctx.moveTo(900, 420);                 
+			ctx.lineTo(870,420);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();
+
+			ctx.moveTo(880, 465);                 
+			ctx.lineTo(850,465);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();
+			
+			ctx.moveTo(850, 198);                 
+			ctx.lineTo(900,198);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();
+			//horizontal
+
+			ctx.moveTo(850,130);                 
+			ctx.lineTo(850,280);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();
+			//vertical
+
+            ctx.moveTo(720,210);                 
+			ctx.lineTo(720,270);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();
+
+			ctx.moveTo(820, 270);                 
+			ctx.lineTo(720,270);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();
+
+			ctx.moveTo(760, 330);                 
+			ctx.lineTo(690,330);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();
+
+			ctx.moveTo(775,115);                 
+			ctx.lineTo(775,200);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();
+
+			ctx.moveTo(650, 115);                 
+			ctx.lineTo(775,115);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();
+
+			ctx.moveTo(650, 115);                 
+			ctx.lineTo(775,115);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();
+
+			ctx.moveTo(590, 335);                 
+			ctx.lineTo(665,335);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();
+
+			ctx.moveTo(600, 275);                 
+			ctx.lineTo(670,275);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();
+
+			ctx.moveTo(550,100);                 
+			ctx.lineTo(550,180);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();
+
+			ctx.moveTo(550,100);                 
+			ctx.lineTo(550,180);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();
+
+			ctx.moveTo(850, 250);                 
+			ctx.lineTo(780,250);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();
+
+			ctx.moveTo(810,330);                 
+			ctx.lineTo(810,460);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();
+
+			ctx.moveTo(870, 330);                 
+			ctx.lineTo(810,330);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();
+			
+
+			ctx.moveTo(747,270);                 
+			ctx.lineTo(747,310);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();
+
+			ctx.moveTo(690,256);                 
+			ctx.lineTo(690,310);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();
+
+			ctx.moveTo(600,400);                 
+			ctx.lineTo(600,450);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();
+
+			ctx.moveTo(530,275);                 
+			ctx.lineTo(530,320);                 
+			ctx.strokeStyle= "black";               
+			ctx.stroke();
+
+			ctx.moveTo(530, 275);                 
+			ctx.lineTo(520,275);                 
 			ctx.strokeStyle= "black";               
 			ctx.stroke();
